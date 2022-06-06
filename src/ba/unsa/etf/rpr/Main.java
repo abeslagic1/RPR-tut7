2b21d573591da0bc8584adf92f004377ff2ac834
Amar Beslagic <abeslagic1@etf.unsa.ba>
2022-06-06 14:33:06 +0200

    Postavljeno graficko okruzenje.

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -15,7 +17,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setTitle("Korisnici");
+        stage.setMinWidth(500);
+        stage.setMinHeight(300);
         stage.toFront();
         stage.show();
     }
